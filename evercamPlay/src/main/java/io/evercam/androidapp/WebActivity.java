6585f16bacac57a45d21acfc4b91165d8736e66f
Liuting Du <kangtadlt@gmail.com>
2015-12-18 10:19:26 +0000

    Save snapshot from cloud recording. Closes #96

--- evercamPlay/src/main/java/io/evercam/androidapp/WebActivity.java
+++ evercamPlay/src/main/java/io/evercam/androidapp/WebActivity.java
@@ -2,13 +2,15 @@ package io.evercam.androidapp;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import io.evercam.androidapp.custom.CustomProgressDialog;
 
+/**
+ * The base web activity with basic loading animation
+ */
 public abstract class WebActivity  extends ParentAppCompatActivity
 {
     private final String TAG = "WebActivity";
@@ -48,36 +50,21 @@ public abstract class WebActivity  extends ParentAppCompatActivity
 
     public WebViewClient getWebViewClient()
     {
-        WebViewClient client = new WebViewClient()
-        {
-            public void onPageStarted(WebView view, String url, Bitmap favicon)
-            {
-                progressDialog.show(WebActivity.this.getString(R.string.msg_loading));
-            }
-
-            public void onPageFinished(WebView view, String url)
-            {
-                progressDialog.dismiss();
-            }
-
-            //TODO: Refactor the code - currently all web view subclasses are using
-            // the same WebViewClient.
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                Log.e(TAG, "shouldOverrideUrlLoading " + url);
-
-                if(url.startsWith("data:image/jpeg;"))
-                {
-                    //TODO: Decode base64 and save the image
-
-                    return true;
-                }
-
-                return super.shouldOverrideUrlLoading(view, url);
-            }
-        };
-        return client;
+        return new BaseWebViewClient();
     }
 
     protected abstract void loadPage();
+
+    protected class BaseWebViewClient extends WebViewClient
+    {
+        public void onPageStarted(WebView view, String url, Bitmap favicon)
+        {
+            progressDialog.show(WebActivity.this.getString(R.string.msg_loading));
+        }
+
+        public void onPageFinished(WebView view, String url)
+        {
+            progressDialog.dismiss();
+        }
+    }
 }
--- evercamPlay/src/main/java/io/evercam/androidapp/recordings/RecordingWebActivity.java
+++ evercamPlay/src/main/java/io/evercam/androidapp/recordings/RecordingWebActivity.java
@@ -1,9 +1,17 @@
 package io.evercam.androidapp.recordings;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
+import io.evercam.Snapshot;
 import io.evercam.androidapp.R;
 import io.evercam.androidapp.WebActivity;
+import io.evercam.androidapp.photoview.SnapshotManager;
+import io.evercam.androidapp.tasks.CaptureSnapshotRunnable;
 import io.evercam.androidapp.tasks.ValidateRightsRunnable;
 import io.evercam.androidapp.utils.Constants;
 
@@ -11,6 +19,8 @@ import io.evercam.androidapp.utils.Constants;
 public class RecordingWebActivity extends WebActivity
 {
     private final String TAG = "RecordingWebActivity";
+
+    private String cameraId = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -35,7 +45,7 @@ public class RecordingWebActivity extends WebActivity
     @Override
     protected void loadPage()
     {
-        String cameraId = bundle.getString(Constants.BUNDLE_KEY_CAMERA_ID);
+        cameraId = bundle.getString(Constants.BUNDLE_KEY_CAMERA_ID);
 
         //Validate if the user still has access to the camera
         new Thread(new ValidateRightsRunnable(this, cameraId)).start();
@@ -50,5 +60,34 @@ public class RecordingWebActivity extends WebActivity
     {
         super.onDestroy();
         setResult(Constants.RESULT_TRUE);
+    }
+
+    @Override
+    public WebViewClient getWebViewClient()
+    {
+        return new CloudRecordingWebViewClient();
+    }
+
+    private class CloudRecordingWebViewClient extends BaseWebViewClient
+    {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url)
+        {
+            Log.e(TAG, "shouldOverrideUrlLoading " + url);
+
+            if(url.startsWith("data:image/jpeg;"))
+            {
+                String dataString = Snapshot.getBase64DataStringFrom(url);
+                byte[] imageData = Snapshot.getDataFrom(dataString);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(imageData, 0, imageData.length);
+
+                new Thread(new CaptureSnapshotRunnable(RecordingWebActivity.this, cameraId, SnapshotManager
+                        .FileType.JPG, bitmap)).start();
+
+                return true;
+            }
+
+            return super.shouldOverrideUrlLoading(view, url);
+        }
     }
 }
